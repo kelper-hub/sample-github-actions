bcf801a4a05b9e6bd26ea0d6b9cd4d1eae5e36ad
blazar00 <braxtonlazar@gmail.com>
2024-11-27 21:08:57 -0800

    Fixing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
 import unittest
 from app.main import greet
 
+
 class TestGreetFunction(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
         self.assertEqual(greet("GitHub"), "Hello, GitHub!")
+
 
 if __name__ == "__main__":
     unittest.main()
